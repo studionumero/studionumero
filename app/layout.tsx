3b6cbb5dfdb97f36d05d5c0788727ba018f2aeec
Glenn <glenn@studionumero.com>
2024-07-06 20:58:14 -0700

    Added test images and added microsoft clarity script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <GoogleAnalytics gaId="G-XLNFETQQZD" />
+      <GoogleAnalytics gaId="G-XLNFETQQZD" />
+      <head>
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "n3g5vu415n");
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
